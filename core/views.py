3237116a00d9b66dc5231c23f35a65e4387aba61
djumanov <diyorbekjumanov1120@gmail.com>
2024-05-15 11:42:53 +0500

    landing page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
-
-from users.models import User
 from users.client import oAuth2Client
-from dashboard.models import Submission, Department, Metric
 
 
 def landing_page(request):
@@ -17,10 +14,6 @@ def landing_page(request):
     )
 
     ctx = {
-        'users': User.objects.all(),
-        'submissions': Submission.objects.all(),
-        'departments': Department.objects.all(),
-        'metrics': Metric.objects.all(),
         'authorization_url': client.get_authorization_url()
     }
     return render(request, 'landing.html', ctx)
