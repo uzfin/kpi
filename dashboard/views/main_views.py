47f850da96184689e5418ac39cadec5361d0770f
djumanov <diyorbekjumanov1120@gmail.com>
2024-05-16 08:30:34 +0500

    admin main dashboard

--- dashboard/views/main_views.py
+++ dashboard/views/main_views.py
@@ -5,120 +5,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.http import HttpRequest, HttpResponse
 from users.models import User, Department
-from dashboard.models import KPI, Submission
+from dashboard.models import KPI
 
 
 class DashboardView(LoginRequiredMixin, View):
     
     def get(self, request: HttpRequest) -> HttpResponse:
-        user: User = request.user
+        user = request.user
 
-        # get kpi
-        kpi_id = request.GET.get('kpi_id')
-        if kpi_id:
-            try:
-                current_kpi = KPI.objects.get(id=kpi_id)
-            except KPI.DoesNotExist:
-                messages.error(request, "Siz tanlagan KPI da xatolik yuz berdi. Iltimos yana bir bor ununib ko'ring.")
-                current_kpi = KPI.objects.order_by("-end_date").first()
-        else:
-            current_kpi = KPI.objects.order_by("-end_date").first()
-            
         if user.role == User.ADMIN:
             ctx = {
-                "users_count": User.objects.exclude(role=User.ADMIN).count(),
-                "department_count": Department.objects.all(),
+                "kpis": KPI.objects.all(),
+                "departments": Department.objects.all(),
+                "employees": User.objects.filter(role=User.EMPLOYEE),
             }
-            if current_kpi:
-                ctx["other_kpis"] = KPI.objects.exclude(id=current_kpi.id),
-                ctx["current_kpi"] = current_kpi,
-                ctx["submissions_count"] = current_kpi.submissions.count(),
-                ctx["clauses_count"] = current_kpi.clauses.count(),
-                ctx["criterions_count"] = current_kpi.criterions.count(),
 
             return render(request, "dashboard/main/admin.html", ctx)
-            
-
-        # if user.role == User.MANAGER:
-        #     kpis = KPI.objects.filter(responsible_employee=user)
-        # else:
-        #     kpis = KPI.objects.all()
-
-        # pairs = []
-        # if user.role == User.EMPLOYEE:
-        #     for i in range(0, len(kpis), 2):
-        #         if len(pairs) == 2: break
-
-        #         if i + 1 < len(kpis):
-        #             pairs.append((user.get_kpi_status(kpis[i]), user.get_kpi_status(kpis[i + 1])))
-        #         else:
-        #             pairs.append((user.get_kpi_status(kpis[i]),))
-        # else:
-        #     users = User.objects.filter(role=User.EMPLOYEE)
-        #     n = len(users)
-            
-        #     for i in range(0, len(kpis), 2):
-        #         if len(pairs) == 2: break
-
-        #         if i + 1 < len(kpis):
-        #             percent1, percent2 = 0, 0
-        #             ball1, ball2 = 0, 0
-                    
-        #             for user in users:
-        #                 data1 = user.get_kpi_status(kpis[i])
-        #                 percent1 += data1['percent']
-        #                 ball1 += data1['ball']
-
-        #                 data2 = user.get_kpi_status(kpis[i + 1])
-        #                 percent2 += data2['percent']
-        #                 ball2 += data2['ball']
-
-        #             pairs.append(
-        #                 (
-        #                     {
-        #                         "id": kpis[i].id,
-        #                         "name": kpis[i].name,
-        #                         "percent": percent1,
-        #                         "total": kpis[i].total_ball,
-        #                         "ball": round(ball1 / n)
-        #                     },
-        #                     {
-        #                         "id": kpis[i + 1].id,
-        #                         "name": kpis[i + 1].name,
-        #                         "percent": percent2,
-        #                         "total": kpis[i + 1].total_ball,
-        #                         "ball": round(ball2 / n)
-        #                     }
-        #                 )
-        #             )
-        #         else:
-        #             percent = 0
-        #             ball = 0
-                    
-        #             for user in users:
-        #                 data = user.get_kpi_status(kpis[i])
-        #                 percent += data['percent']
-        #                 ball += data['ball']
-
-        #             pairs.append(
-        #                 (
-        #                     {
-        #                         "id": kpis[i].id,
-        #                         "name": kpis[i].name,
-        #                         "percent": percent,
-        #                         "total": kpis[i].total_ball,
-        #                         "ball": round(ball / n)
-        #                     },
-        #                 )
-        #             )
-
-        #     # TODO: status of departments
-
-        # ctx = {
-        #     "kpis": kpis,
-        #     "pairs": pairs,
-        #     "departments": Department.objects.all()
-        # }
-
-        # return render(request, 'dashboard/main.html', ctx)
-
