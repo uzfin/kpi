b40283232ac680dbda2570f63f03f975114dc7e0
djumanov <diyorbekjumanov1120@gmail.com>
2024-05-29 10:50:34 +0500

    static root and collect static

--- core/settings.py
+++ core/settings.py
@@ -141,6 +141,7 @@ LOGIN_URL = 'users:login'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
